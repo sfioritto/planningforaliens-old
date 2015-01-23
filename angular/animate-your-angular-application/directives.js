55ca7e9231d9c4a8b10b0771965a92ee2fbfd656
= <sean.fioritto@gmail.com>
2015-01-23 17:07:51 -0600

    first version done.

--- angular/animate-your-angular-application/directives.js
+++ angular/animate-your-angular-application/directives.js
@@ -1,6 +1,6 @@
-var app = angular.module('animations', []);
+var app = angular.module('animations', ['ngAnimate']);
 
-app.controller("example1", function($scope){
+app.controller("example", function($scope){
     $scope.awesome = false;
 });
 
@@ -25,10 +25,10 @@ app.directive("myHideAnimated", function(){
         link: function(scope, elem, attrs){
             scope.$watch(attrs.myHideAnimated, function(value){
                 if (value) {
-                    elem.addClass("hide-add");
+                    elem.addClass("hide-add-start");
                     setTimeout(function() {
                         elem.addClass("hide");
-                        elem.removeClass("hide-add");
+                        elem.removeClass("hide-add-start");
                     }, 1000);
                 } else {
                     elem.addClass("hide-remove");
@@ -44,4 +44,50 @@ app.directive("myHideAnimated", function(){
             });
         }
     };
+});
+
+app.directive("myHideNgAnimate", function($animate){
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs){
+            scope.$watch(attrs.myHideNgAnimate, function(value){
+                if (value) {
+                    $animate.addClass(elem, "hide");
+                } else {
+                    $animate.removeClass(elem, "hide");
+                }
+            });
+        }
+    };
+});
+
+app.directive("myHideJs", function($animate){
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs){
+            scope.$watch(attrs.myHideJs, function(value){
+                if (value) {
+                    $animate.addClass(elem, "hide-js");
+                } else {
+                    $animate.removeClass(elem, "hide-js");
+                }
+            });
+        }
+    };
+});
+
+
+app.animation('.hide-js-animated', function(){
+    return {
+        addClass: function(element, className){
+            TweenMax.to(element, 1, {
+                'opacity': 0
+                });
+        },
+        removeClass: function(element, className){
+            TweenMax.to(element, 1, {
+                'opacity': 1
+            });
+        }
+    }
 });
