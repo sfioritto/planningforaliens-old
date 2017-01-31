5c206dd483d821d85b5a4bda6e7bd767fa50fc7c
Sean Fioritto <sean@planningforaliens.com>
2017-01-31 15:15:03 -0600

    updated focus.js

--- focus/focus.js
+++ focus/focus.js
@@ -25,7 +25,7 @@ $(function(){
         strokeColor: "rgba(220,220,220,0.8)",
         highlightFill: "rgba(220,220,220,0.75)",
         highlightStroke: "rgba(220,220,220,1)",
-        data: [0, 0, 0, 0, 9000, 9000, 9000, 4500, 4500]
+        data: [0, 0, 0, 0, 3000, 9000, 9000, 4500, 4500]
       },
       {
         label: "My Second dataset",
@@ -33,7 +33,7 @@ $(function(){
         strokeColor: "rgba(151,187,205,0.8)",
         highlightFill: "rgba(151,187,205,0.75)",
         highlightStroke: "rgba(151,187,205,1)",
-        data: [4000, 3500, 5500, 15000, 0, 0, 0, 0, 0]
+        data: [4000, 3500, 5500, 15000, 6000, 0, 0, 0, 0]
       }
     ]
   };
