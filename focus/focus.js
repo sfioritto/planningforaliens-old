3e36c0cb46c77582c407457107d6c3ed3a4db242
Sean Fioritto <sean@planningforaliens.com>
2017-03-20 13:04:18 -0500

    change focus to put hsa first.

--- focus/focus.js
+++ focus/focus.js
@@ -12,8 +12,8 @@ $(function(){
              "Window Treatments",
              "Garage",
              "Tuck Pointing",
-             "1 Month Buffer",
              "2016 HSA",
+             "1 Month Buffer",
              "1 Month Buffer",
              "1 Month Buffer",
              "2017 HSA",
@@ -27,7 +27,7 @@ $(function(){
         strokeColor: "rgba(220,220,220,0.8)",
         highlightFill: "rgba(220,220,220,0.75)",
         highlightStroke: "rgba(220,220,220,1)",
-        data: [0, 0, 0, 0, 1000, 6750, 9000, 9000, 6750, 14000, 4500]
+        data: [0, 0, 0, 0, 0, 5000, 9000, 9000, 6750, 14000, 4500]
       },
       {
         label: "My Second dataset",
@@ -35,7 +35,7 @@ $(function(){
         strokeColor: "rgba(151,187,205,0.8)",
         highlightFill: "rgba(151,187,205,0.75)",
         highlightStroke: "rgba(151,187,205,1)",
-        data: [4000, 3500, 5500, 15000, 8000, 0, 0, 0, 0, 0, 0]
+        data: [4000, 3500, 5500, 15000, 5000, 4000, 0, 0, 0, 0, 0]
       }
     ]
   };
